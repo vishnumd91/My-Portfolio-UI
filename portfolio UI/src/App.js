2946a64e9659f9138e81f041d80f231d63971f31
vishnumd91 <vishnudivakaran1@gmail.com>
2021-01-26 22:13:10 +0530

    Initial webpack and babel setup done for basic react coding

---
+++ portfolio UI/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+function App() {
+    return (
+        <div>
+            <h1 className='a' style={{color: 'red'}}>My Portfolio</h1>
+
+        </div>
+    )
+}
+
+export default App
