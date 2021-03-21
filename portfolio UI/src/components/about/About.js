518b1a67a05747c00566ef4c34fb53d9104be0bb
vishnumd91 <vishnudivakaran1@gmail.com>
2021-03-21 16:33:09 +0530

    Added Static about component

---
+++ portfolio UI/src/components/about/About.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className="container-fluid p-0">
+            {/* <!-- About--> */}
+            <section className="resume-section" id="about">
+                <div className="resume-section-content">
+                    <h1 className="mb-0">
+                        VISHNU
+                        <span className="text-primary"> M D</span>
+                    </h1>
+                    <div className="subheading mb-5">
+                        120 Malayamchath House · Chalissery, PO 679536 · (+91) 9605774685 ·
+                        <a href="mailto:name@email.com">VISHNUDIVAKARAN1@GMAIL.COM</a>
+                    </div>
+                    <p className="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+                    <div className="social-icons">
+                        <a className="social-icon" href="#"><i className="fab fa-linkedin-in"></i></a>
+                        <a className="social-icon" href="#"><i className="fab fa-github"></i></a>
+                        <a className="social-icon" href="#"><i className="fab fa-twitter"></i></a>
+                        <a className="social-icon" href="#"><i className="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+    </div>
+  )
+}
+
+export default About
