3e764c5357378938f143642d5e863f3e488d4c15
vishnumd91 <vishnudivakaran1@gmail.com>
2021-05-02 17:17:44 +0530

    Added experience component and amde some enhancements to routing

--- portfolio UI/src/App.js
+++ portfolio UI/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import './index.css';
-import { Route, Switch } from 'react-router-dom';
+
 
 import Navigation from './components/navigation/Navigation';
-import About from './components/about/About';
-import Education from './components/education/Education';
 function App() {
     return (
         <div>
-          <Switch>
-            <Route exact path = '/'>
-              <Navigation />
-              <About></About>
-            </Route>
-
-            <Route path = '/about'>
-              <About />
-            </Route>
-            <Route path = '/education'>
-              <Education />
-            </Route>
-          </Switch>
+          <Navigation />
         </div>
     )
 }
