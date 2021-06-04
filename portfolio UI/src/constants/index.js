a767db74083a5c461f69848070c7f0cf44696fd6
vishnumd91 <vishnudivakaran1@gmail.com>
2021-06-04 16:32:42 +0530

    Code cleanup for Experience component

--- portfolio UI/src/constants/index.js
+++ portfolio UI/src/constants/index.js
@@ -20,3 +20,25 @@ export const navigationData = [
     'heading': 'Interests',
   },
 ];
+
+export const experienceData = [
+  {
+    heading : 'Senior Web Developer',
+    subHeading : 'Intelitec Solutions',
+    content : 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.',
+    workExperience : 'March 2013 - Present'
+  },
+  {
+    heading : 'Web Developer',
+    subHeading : 'Intelitec Solutions',
+    content : 'Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.',
+    workExperience : 'December 2011 - March 2013'
+  },
+  {
+    heading : 'Junior Web Designer',
+    subHeading : 'Shout! Media Productions',
+    content : 'Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.',
+    workExperience : 'July 2010 - December 2011'
+  }
+
+];
