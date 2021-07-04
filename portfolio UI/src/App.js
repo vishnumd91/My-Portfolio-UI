a95afb91c58b5dc891d4ee43e95f7fae5c10fbb0
Vishnu MD <vishnudivakaran1@gmail.com>
2021-07-04 17:03:45 +0530

    routing addition for admin

--- portfolio UI/src/App.js
+++ portfolio UI/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './index.css';
 
+import { Route, Switch } from 'react-router-dom';
+import Admin from './components/admin/Admin';
+
 
 import Navigation from './components/navigation/Navigation';
+
 function App() {
     return (
         <div>
-          <Navigation />
+          <Switch>
+          <Route exact path = '/' component = {Navigation} />
+          <Route path = '/admin' component = {Admin} />
+          </Switch>
         </div>
     )
 }
