71f92811a5c941d358c90da4d57296b68ed46bb5
Vishnu MD <vishnudivakaran1@gmail.com>
2021-07-04 17:09:55 +0530

    reverting routing change fro admin

--- portfolio UI/src/App.js
+++ portfolio UI/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './index.css';
 
-import { Route, Switch } from 'react-router-dom';
-import Admin from './components/admin/Admin';
-
 
 import Navigation from './components/navigation/Navigation';
-
 function App() {
     return (
         <div>
-          <Switch>
-          <Route exact path = '/' component = {Navigation} />
-          <Route path = '/admin' component = {Admin} />
-          </Switch>
+          <Navigation />
         </div>
     )
 }
