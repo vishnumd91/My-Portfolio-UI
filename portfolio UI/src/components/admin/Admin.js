823bce3047ce9d88936b79b285a72643e59c82dd
Vishnu MD <vishnudivakaran1@gmail.com>
2021-07-06 23:31:24 +0530

    created admin login page with new route

--- portfolio UI/src/components/admin/Admin.js
+++ portfolio UI/src/components/admin/Admin.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import "./Admin.css";
 
 const Admin = () => {
   return (
-    <div>
-      <h1>I am an Admin</h1>
+    <div className="wrapper">
+      <div className="heading">
+        <span className="title">Admin Login</span>
+      </div>
+      <div className="formStyle">
+        <form>
+          <div className="form-group">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Username"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <br></br>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
