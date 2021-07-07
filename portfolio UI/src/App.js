94540208ff6738944b29c20d08e075a9a79e7534
Vishnu MD <vishnudivakaran1@gmail.com>
2021-07-07 22:32:24 +0530

    created admin login page with new route (#10)
    * created admin login page with new route
    
    * added login functionality

--- portfolio UI/src/App.js
+++ portfolio UI/src/App.js
@@ -3,7 +3,8 @@ import { Route, Switch} from "react-router-dom";
 import "./index.css";
 
 import Navigation from "./components/navigation/Navigation";
-import Admin from "./components/admin/Admin";
+import Admin from "./components/admin/login/AdminLogin";
+import Dashboard from "./components/admin/dashboard/Dashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Navigation} />
         <Route path="/admin" component={Admin} />
+        <Route path='/dashboard' component={Dashboard} />
       </Switch>
     </div>
   );
