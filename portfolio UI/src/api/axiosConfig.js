8f37635dd2d9a9099a320707995dfb6e3814dea4
vishnumd91 <vishnudivakaran1@gmail.com>
2022-06-25 21:02:25 +0530

    Fixed babel issue and tested backend data

--- portfolio UI/src/api/axiosConfig.js
+++ portfolio UI/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://portfolio-nodejs-api.herokuapp.com/",
-  timeout: 1000,
+  // timeout: 3000,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
