ab9af0e34cad742f5185826b357a7e8db78f6b22
Vishnu MD <vishnudivakaran1@gmail.com>
2022-06-25 21:09:30 +0530

    Axios configuration (#16)
    * axios configuration with babel regenerator error
    
    * Fixed babel issue and tested backend data

--- portfolio UI/src/components/experience/Experience.js
+++ portfolio UI/src/components/experience/Experience.js
@@ -1,32 +1,52 @@
-import React from 'react';
-import { experienceData } from '/src/constants';
-
+import React, { useEffect, useState } from "react";
+import { experienceData } from "/src/constants";
+import axiosInstance from "../../api/axiosConfig";
 
 function Experience() {
-
   const experience = experienceData || {};
+  // Server Data will be used in DOM once proper API implementation is done.
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axiosInstance.get("/portfolio/experience");
+    setData(response.data);
+  };
+  // eslint-disable-next-line no-undef
+  console.log("test exp data", data);
 
   return (
-    <div className = 'container-fluid p-0'>
-            <section className="resume-section" id="experience">
-                <div className="resume-section-content">
-                    <h2 className="mb-5">Experience</h2>
-                    {experience.map((experienceInputs, index) => {
-                      return (
-                        <div key={index} className="d-flex flex-column flex-md-row justify-content-between mb-5">
-                          <div className="flex-grow-1">
-                            <h3 className="mb-0">{experienceInputs.heading}</h3>
-                            <div className="subheading mb-3">{experienceInputs.subHeading}</div>
-                            <p>{experienceInputs.content}</p>
-                          </div>
-                          <div className="flex-shrink-0"><span className="text-primary">{experienceInputs.workExperience}</span></div>
-                        </div>
-                      );
-                    })}
+    <div className="container-fluid p-0">
+      <section className="resume-section" id="experience">
+        <div className="resume-section-content">
+          <h2 className="mb-5">Experience</h2>
+          {experience.map((experienceInputs, index) => {
+            return (
+              <div
+                key={index}
+                className="d-flex flex-column flex-md-row justify-content-between mb-5"
+              >
+                <div className="flex-grow-1">
+                  <h3 className="mb-0">{experienceInputs.heading}</h3>
+                  <div className="subheading mb-3">
+                    {experienceInputs.subHeading}
+                  </div>
+                  <p>{experienceInputs.content}</p>
                 </div>
-            </section>
+                <div className="flex-shrink-0">
+                  <span className="text-primary">
+                    {experienceInputs.workExperience}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
